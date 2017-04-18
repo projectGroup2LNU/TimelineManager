361dd7f9f2b9779c1ae25eda638d8f6173efbbb7
yamaeda64 <joakim_bergqvist@hotmail.com>
2017-04-18 22:38:45 +0200

    refactoring
    Some refactoring, some filenames have been changed, comments added,
    small changes in the classes.

---
+++ ProjectFiles/src/timelineManager/controller/AddTimelineController.java
@@ -0,0 +1,31 @@
+package timelineManager.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+/**
+ * This is a controller class for the AddTimelineView.
+ * it holds the variables and logics needed to create a new timeline from GUI.
+ * @author beysimeryalmaz
+ */
+public class AddTimelineController {
+    
+    @FXML
+    private JFXTextField titleField;
+
+    @FXML
+    private JFXDatePicker startDate;
+
+    @FXML
+    private JFXDatePicker endDate;
+
+    @FXML
+    private JFXTextArea descriptionField;
+
+    @FXML
+    private JFXButton saveButton;
+    
+}
