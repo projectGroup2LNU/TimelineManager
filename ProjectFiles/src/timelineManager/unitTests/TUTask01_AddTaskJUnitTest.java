2e85ad918e44f78c51912a2479b20590e771119c
be222dd <beysimeryilmaz@gmail.com>
2017-04-29 20:49:53 +0200

    :wq

---
+++ ProjectFiles/src/timelineManager/unitTests/TUTask01_AddTaskJUnitTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timelineManager.unitTests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import timelineManager.model.Task;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import javafx.scene.paint.Color;
+import java.util.ArrayList;
+
+public class TUTask01_AddTaskJUnitTest {
+	private ArrayList<Task> tasks = new ArrayList<Task>();
+	
+	@Test
+	public void testTasks() {
+		addTasks(1);
+			
+		// Tests only one task
+		assertEquals("TestTitle0", tasks.get(0).getTitle());
+		assertEquals("TestDescription0", tasks.get(0).getDescription());
+		assertEquals(LocalDate.now(), tasks.get(0).getStartTime().toLocalDate());
+		assertEquals(LocalDate.now().plusDays(1), tasks.get(0).getEndTime().toLocalDate());
+		assertEquals(Color.rgb(0, 0, 0), tasks.get(0).getColor());	
+		assertEquals(1, tasks.get(0).getId());
+		assertEquals("0", tasks.get(0).getPriority());
+		
+		addTasks(100);
+		
+		// Tests first task again after adding more tasks
+		assertEquals("TestTitle0", tasks.get(0).getTitle());
+		assertEquals("TestDescription0", tasks.get(0).getDescription());
+		assertEquals(LocalDate.now(), tasks.get(0).getStartTime().toLocalDate());
+		assertEquals(LocalDate.now().plusDays(1), tasks.get(0).getEndTime().toLocalDate());
+		assertEquals(Color.rgb(0, 0, 0), tasks.get(0).getColor());
+		assertEquals(1, tasks.get(0).getId());
+		assertEquals("0", tasks.get(0).getPriority());
+		
+		// Tests middle task
+		assertEquals("TestTitle50", tasks.get(50).getTitle());
+		assertEquals("TestDescription50", tasks.get(50).getDescription());
+		assertEquals(LocalDate.now().plusDays(50), tasks.get(50).getStartTime().toLocalDate());
+		assertEquals(LocalDate.now().plusDays(51), tasks.get(50).getEndTime().toLocalDate());
+		assertEquals(Color.rgb(50, 0, 0), tasks.get(50).getColor());
+		assertEquals(51, tasks.get(50).getId());
+		assertEquals("50", tasks.get(50).getPriority());
+		
+		// Tests last task
+		assertEquals("TestTitle100", tasks.get(100).getTitle());
+		assertEquals("TestDescription100", tasks.get(100).getDescription());
+		assertEquals(LocalDate.now().plusDays(100), tasks.get(100).getStartTime().toLocalDate());
+		assertEquals(LocalDate.now().plusDays(101), tasks.get(100).getEndTime().toLocalDate());
+		assertEquals(Color.rgb(100, 0, 0), tasks.get(100).getColor());	
+		assertEquals(101, tasks.get(100).getId());
+		assertEquals("100", tasks.get(100).getPriority());
+	}
+	
+	// Adds new tasks to the ArrayList
+	private void addTasks(int amount) {
+		int startNumber = tasks.size();
+		
+		for(int i = startNumber; i < amount + startNumber; i++) {
+			tasks.add(new Task("TestTitle" + i, "TestDescription" + i, LocalDateTime.now().plusDays(i), LocalDateTime.now().plusDays(i + 1), Color.rgb(i,0,0), "" + i));
+		}
+	}
+}
+
